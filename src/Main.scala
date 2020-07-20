a05e9449a1b5700336fe2b145e65cbfd053187dd
Neil <neil.torrero@students.salle.url.edu>
2020-07-20 17:25:26 +0200

    print of matrix

--- src/Main.scala
+++ src/Main.scala
@@ -1,7 +1,22 @@
 object Main {
   def NMatrix = 10
+  def printMatrix(gol: GameOfLife) {
+    for (j <- 0 until NMatrix;
+         i <- 0 until NMatrix) {
+      if (gol.matrix(i)(j)) {
+        print("[x]")
+      } else {
+        print("[ ]")
+      }
+      if (i == NMatrix-1) {
+        print("\n")
+      }
+    }
+  }
+
 
   def main(args: Array[String]) {
     val gol = new GameOfLife(NMatrix)
     gol.initGOL()
+        printMatrix(gol)
 }
